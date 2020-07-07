5ddec3a7b6336871fad63a5789f5e19b74af39cd
Lucas Videla <videla.lucas@gmail.com>
2020-07-07 19:39:19 -0300

    UI improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def peek(text)
+    r = text.to_plain_text
+    return "#{r[0..100]} &mldr;" if r.size > 100
+    r
+  end
+
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,4 +3,8 @@ class Exercise < ApplicationRecord
   has_rich_text :solution
 
   acts_as_taggable_on :tags
+
+  def readable_id
+    "E" + id.to_s.rjust(3, "0")
+  end
 end
